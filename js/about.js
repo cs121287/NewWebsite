d42b0ac6093d671c7573e2f44a0bd0583bf1af26
cs121287 <133766445+cs121287@users.noreply.github.com>
2025-04-01 00:34:11 -0700

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -4,7 +4,7 @@ var rule = CSSRulePlugin.getRule("span:after");
 var tl = gsap.timeline({defaults:{duration: 1}});
 tl.from(".anim1", {y:-50, stagger: .6, opacity: 0})
   .to(rule, {duration: 1.8, cssRule: {scaleY: 0}}, "-=2.2")
-  .from(".about-section", {backgroundPosition: '200px 0px', opacity: 0}, "-=1.5")
+  .from("#about", {backgroundPosition: '200px 0px', opacity: 0}, "-=1.5")
   .from(".about-image img", {y:30, opacity: 0}, "-=.5");
 
 // Add event listener if needed for any interactive elements
